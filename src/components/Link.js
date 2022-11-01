8e6a0b664ea2db048ed7200abb1943182e27dea7
Francis Enemuo <francenemuo@gmail.com>
2022-11-01 15:36:16 +0100

    feat: adds the link sections
    The profile section contents the different links to the sections

---
+++ src/components/Link.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Link() {
+  return (
+    <div className='link-section'>
+        <section>
+            <a href = ""></a>
+        </section>
+        <section>
+            <a href = ""></a>
+        </section>
+        <section>
+            <a href = ""></a>
+        </section>
+        <section>
+            <a href = ""></a>
+        </section>
+        <section>
+            <a href = ""></a>
+        </section>
+        <section>
+            <a href = ""></a>
+        </section>
+
+    </div>
+  )
+}
