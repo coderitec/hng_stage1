5b7f34ae4a05d824689c8e5bb6a90345d7d8d149
Francis Enemuo <francenemuo@gmail.com>
2022-11-01 09:54:40 +0100

    feat: setup files for stage1 project

---
+++ src/components/Image.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import pic from '../images/franc.png'
+import '../App.css'
+
+export default function Image() {
+  return (
+    <div>
+        <img src={pic} alt="Franc smiling" className='img-avatar'/>
+        <h1 className='title'>Franc Enemuo</h1>
+    </div>
+  )
+}
